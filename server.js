9b4692ecacae72826ee0a3a6746384bc65b7a6f3
athulyaliving1 <muthukumar@athulyaliving.com>
2023-06-09 11:28:38 +0530

    api changes

--- server.js
+++ server.js
@@ -6,125 +6,138 @@ import jwt from 'jsonwebtoken';
 import cookieParser from 'cookie-parser';
 import morgan from 'morgan';
 
-const salt=10;
+const salt = 10;
 
-const app=express();
+const app = express();
 app.use(express.json());
 app.use(cors());
 app.use(cookieParser());
-app.use(morgan());
+app.use(morgan('dev'));
 
-const db=mysql.createConnection({
-    host:"localhost",
-    user:"root",
-    password:"root123",
-    database:"theatgg6_sal_subscriber102"
-});
+// const db=mysql.createConnection({
+//     host:"localhost",
+//     user:"root",
+//     password:"root123",
+//     database:"theatgg6_sal_subscriber102"
+// });
 
-app.listen(8081,()=>{
 
-    console.log("Running in port 8081...");
-});
-
-app.post("/login",(req,res)=>{
-
-    const sql="select * from complaint_users where email=?";
-    const values=[
-        req.body.email,
-    ];
-    console.log(req.body.email);
-    db.query(sql,[values],(err,data)=>{
-
-        if(err)
-        {
-            return res.json({Error:err});
-
-        }
-        if(data.length>0)
-        {
-            console.log(data[0].password);
-            // console.log(req.body.password.toString()==data[0].password);
-            bcrypt.compare(req.body.password.toString(),data[0].password,(err,response)=>{
-                if(err) return res.json({Error:"Password compare error"});
-
-                if(response){
-                    //   return res.json({Status:"success"});
-                      const token = jwt.sign({id:data[0].email},'Athulya',{ expiresIn: '1h' });
-                      return res.status(200).send({
-                        msg: 'Logged in!',
-                        token,
-                        user: data[0]
-                        });
-                        
-                }else{
-                    return res.json({Status:"Password not match"});
-                }
-            });
-        }else{
-            return res.json({Error:"No Email"});
-        }
-        
-    });
-
-});
-
-app.post('/register',(req,res)=>{
-
-    const sql="insert into complaint_users (`name`,`email`,`password`) values(?)";
-    const encryptedPassword =  bcrypt.hash(req.body.password, 10,function(err,pin){
-
-        console.log(pin);
-        const values=[
-            req.body.name,
-            req.body.email,
-            pin
-        ];
-    
-        db.query(sql,[values],(err,result)=>{
-    
-            if(err)
-            {
-                return res.json({Error:err});
-    
-            }
-            return res.json({Status:"success"});
-        });
-        
-    });
-    //const token = jwt.sign({email:'mageshema1180@gmail.com'},'iuc',{ expiresIn: '1h' });
-    // req.body.username="Karan";
-    // req.body.password=token;
-    // req.body.email="dsfg@gmail.com";
-    
-   
-    console.log(encryptedPassword+"iii");
-   
-
-    
-   
-
-   
+var db = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "nodeapp"
 });
 
 
-app.post("/getpage",(req,res)=>{
 
-    if(!req.headers.authorization || !req.headers.authorization.startsWith('Bearer') || !req.headers.authorization.split(' ')[1]){
-        
-        return res.status(422).json({
-        message: "Please provide the token",
-        });
-        
+app.listen(8081, () => {
+
+  console.log("Running in port 8081...");
+});
+
+app.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  console.log(email);
+  console.log(password);
+  // Check if the user exists in the database
+  const sql = "SELECT * FROM complaint_users WHERE email = ?";
+  db.query(sql, [email], (err, results) => {
+    if (err) {
+      console.error("Error executing the query: ", err);
+      return res.status(500).json({ message: "Internal Server Error" });
     }
 
-    const theToken = req.headers.authorization.split(' ')[1];
-    const decoded = jwt.verify(theToken, 'Athulya');
-    console.log(decoded);
-    db.query('SELECT * FROM complaint_users where email=?', decoded.id, function (error, results, fields) {
+    if (results.length === 0) {
+      return res.status(404).json({ message: "User not found" });
+    }
+
+    // Compare the password with the hashed password stored in the database
+    const user = results[0];
+    bcrypt.compare(password, user.password, (bcryptErr, bcryptResult) => {
+      if (bcryptErr) {
+        console.error("Error comparing passwords: ", bcryptErr);
+        return res.status(500).json({ message: "Internal Server Error" });
+      }
+
+      if (!bcryptResult) {
+        return res.status(401).json({ message: "Invalid password" });
+      }
+
+      // Create and sign a JSON Web Token (JWT)
+      const token = jwt.sign({ id: user.id, email: user.email }, "Athulya", {
+        expiresIn: "1h",
+      });
+
+      return res.json({ token });
+    });
+  });
+});
+
+
+app.post('/register', async (req, res) => {
+  try {
+    const { username, email, mobile, password } = req.body;
+
+    // Check if the user already exists in the database
+    const checkUserSql = 'SELECT * FROM complaint_users WHERE email = ?';
+    db.query(checkUserSql, [email], (err, results) => {
+      if (err) {
+        console.error('Error executing the query:', err);
+        return res.status(500).json({ error: 'Internal Server Error' });
+      }
+
+      if (results.length > 0) {
+        // User with the provided email already exists
+        return res.status(409).json({ error: 'User already exists' });
+      }
+
+      // User is not registered, proceed with registration
+      bcrypt.hash(password, 10, (bcryptErr, hashedPassword) => {
+        if (bcryptErr) {
+          console.error('Error hashing password:', bcryptErr);
+          return res.status(500).json({ error: 'Internal Server Error' });
+        }
+
+        const registerSql = 'INSERT INTO complaint_users (`username`, `email`, `mobile`, `password`) VALUES (?, ?, ?, ?)';
+        const registerValues = [username, email, mobile, hashedPassword];
+
+        db.query(registerSql, registerValues, (registerErr, result) => {
+          if (registerErr) {
+            console.error('Error executing the query:', registerErr);
+            return res.status(500).json({ error: 'Internal Server Error' });
+          }
+
+          return res.json({ status: 'success' });
+        });
+      });
+    });
+  } catch (error) {
+    console.error('Error processing the registration:', error);
+    return res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+app.post("/getpage", (req, res) => {
+
+  if (!req.headers.authorization || !req.headers.authorization.startsWith('Bearer') || !req.headers.authorization.split(' ')[1]) {
+
+    return res.status(422).json({
+      message: "Please provide the token",
+    });
+
+  }
+
+  const theToken = req.headers.authorization.split(' ')[1];
+  const decoded = jwt.verify(theToken, 'Athulya');
+  console.log(decoded);
+  db.query('SELECT * FROM complaint_users where email=?', decoded.id, function (error, results, fields) {
     if (error) {
-        throw error;
+      throw error;
     }
     return res.send({ error: false, data: decoded, message: 'Fetch Successfully.' });
-    });
+  });
 
 });
